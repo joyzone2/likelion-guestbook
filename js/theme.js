458c83f8ef2b28ba13c626a1e1cfde56d2db8dc1
jisukim <joyzone2@naver.com>
2026-05-31 12:49:55 +0900

    style: 방명록 화면 스타일 수정

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,6 @@
 const app = document.querySelector("#app");
 const writeTitle = document.querySelector("#writeTitle");
 const listTitle = document.querySelector("#listTitle");
-const writeSideLeft = document.querySelector("#writeSideLeft");
 const writeSideRight = document.querySelector("#writeSideRight");
 
 function setThemeByTime() {
@@ -10,23 +9,20 @@ function setThemeByTime() {
   app.classList.remove("theme-sunset");
   app.classList.remove("theme-night");
 
-  if (hour >= 6 && hour < 12) {
+  if (hour >= 6 && hour < 13) {
     app.classList.add("theme-day");
     writeTitle.textContent = "🌻방명록을 남겨주세요!🌻";
     listTitle.textContent = "방명록 기록☀️";
-    writeSideLeft.textContent = "오전모드입니다";
     writeSideRight.textContent = "It's Light Mode";
-  } else if (hour >= 12 && hour < 18) {
+  } else if (hour >= 13 && hour < 18) {
     app.classList.add("theme-sunset");
     writeTitle.textContent = "💌방명록을 남겨주세요!💌";
     listTitle.textContent = "방명록 기록✨";
-    writeSideLeft.textContent = "오후모드입니다";
     writeSideRight.textContent = "It's Sunset Mode";
   } else {
     app.classList.add("theme-night");
     writeTitle.textContent = "💫방명록을 남겨주세요!💫";
     listTitle.textContent = "방명록 기록🌙";
-    writeSideLeft.textContent = "저녁모드입니다";
     writeSideRight.textContent = "It's Dark Mode";
   }
 }
